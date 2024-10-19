4c886038eb1b6b2fbc6015d24d1930a212d9eb58
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-10-19 15:09:38 +0500

    Add repo interfaces

---
+++ dasturlash.uz/src/main/java/dasturlash/uz/repository/ArticleTypeRepository.java
@@ -0,0 +1,7 @@
+package dasturlash.uz.repository;
+
+import dasturlash.uz.entity.ArticleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleTypeRepository extends JpaRepository<ArticleType, Long> {
+}
