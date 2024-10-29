09278299628d5ecd20dbdf5cd83822c2a74f2e2c
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-10-29 20:40:22 +0500

    fix:Add SpringSecurityConfig

---
+++ dasturlash.uz/src/main/java/dasturlash/uz/config/SpringSecurityConfig.java
@@ -0,0 +1,57 @@
+package dasturlash.uz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // authentication - Foydalanuvchining identifikatsiya qilish.
+        // Ya'ni berilgan login va parolli user bor yoki yo'qligini aniqlash.
+        String password = UUID.randomUUID().toString();
+        System.out.println("User Password Mazgi: " + password);
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}" + password)
+                .roles("USER")
+                .build();
+
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(user));
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // authorization - Foydalanuvchining tizimdagi huquqlarini tekshirish.
+        // Ya'ni foydalanuvchi murojat qilayotgan API-larni ishlatishga ruxsati bor yoki yo'qligini tekshirishdir.
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/profile").hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated();
+        }).formLogin(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable); // csrf yoqilgan
+        http.cors(Customizer.withDefaults()); // cors yoqilgan
+
+        return http.build();
+    }
+
+}
