97fbb9d7a5d7b8dd87cec4859eb19d92cc41d1d6
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-01 00:03:07 +0500

    fix: Add SpringSecurityUtil

---
+++ dasturlash.uz/src/main/java/dasturlash/uz/util/SpringSecurityUtil.java
@@ -0,0 +1,27 @@
+package dasturlash.uz.util;
+
+import dasturlash.uz.config.CustomUserDetails;
+import dasturlash.uz.enums.Role;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SpringSecurityUtil {
+
+    public static void checkRoleExists(String profileRole, Role... requiredRoles) {
+
+    }
+
+    public static CustomUserDetails getCurrentEntity() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetail = (CustomUserDetails) authentication.getPrincipal();
+
+        return userDetail;
+    }
+
+    public static Long getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetail = (CustomUserDetails) authentication.getPrincipal();
+
+        return userDetail.getId()   ;
+    }
+}
